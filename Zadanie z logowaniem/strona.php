75bbfd24532604e3adce0ff27b1c60649e02fea9
Dominik Szwed <ace19312@gmail.com>
2022-06-07 19:02:32 +0200

    dodanie sesji i naliczania

--- Zadanie z logowaniem/strona.php
+++ Zadanie z logowaniem/strona.php
@@ -1,3 +1,15 @@
+<?php
+session_start();
+
+
+if( isset( $_SESSION['licznik'] ) ) {
+    $_SESSION['licznik'] += 1;
+ }else {
+    $_SESSION['licznik'] = 1;
+ }
+
+ 
+?>
 <!DOCTYPE HTML>
 <html>
 <head>
@@ -6,17 +18,44 @@
 </head>
 <body>
 <?php
+ 
+ 
 $login = $_POST['login'];
 $pass = $_POST['pass'];
-if (!empty($_POST['login']) && !empty($_POST['pass'])){
-    echo("<div class='main'> Witaj ". $login. "<br>");
-    echo('<input type="button" value="wyloguj" onclick="window.location=(\'formularz.html\')"> </div>');
-}
-else{
+if (empty($login) || empty($pass)){
+    
     echo("<script>");
     echo('window.location.replace("formularz.html")');
     echo("</script>");
 }
+else{
+    echo("<div class='main'> Witaj ".$login."<br>");
+    echo('<input type="button" value="Wyloguj" onclick="window.location=(\'formularz.html\')"> ');
+echo('<form method=post>');
+echo("<input type='hidden' name='login' value='$login'>");
+echo("<input type='hidden' name='pass' value='$pass' >");
+echo('<input type="submit" name="submit1" value="Wystartuj Naliczanie Sesji"> ');
+echo('<input type="submit" name="submit2" value="Zatrzymaj Naliczanie Sesji"> ');
+echo('<input type="submit" name="submit3" value="Resetuj Naliczanie Sesji"> ');
+echo('</form>');
+echo("</div>");
+}
+
+
+echo("<p class='licznik'> Odtworzono tą sesję już ".  $_SESSION['licznik'] ." razy </p>");
+
+
+if(isset($_POST['submit2'])){
+    session_reset();
+} 
+if(isset($_POST['submit3'])){
+    $_SESSION['licznik'] = 0;
+    
+} 
+
+
+
 ?>
+
 </body>
 </html>
