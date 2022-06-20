eaf854301539d8428e93fb895a7b9a95e42bcaeb
Dominik Szwed <ace19312@gmail.com>
2022-06-20 17:08:37 +0200

    dodanie opcji zmiany przedmiotu

--- sprawdzanie/index.php
+++ sprawdzanie/index.php
@@ -12,14 +12,13 @@ margin:auto;
 <body>
 <form action="#" method="POST">
 <fieldset>
-   <input type="number" name="id" placeholder="ID">
-   <input type="submit" value="Usuń">
-</fieldset>
-</form>
-<form action="#" method="POST">
-<fieldset>
-   <input type="text" name="itm" placeholder="Przedmiot">
-   <input type="submit" value="dodaj">
+   <input type="number" name="id" placeholder="ID"><br>
+   <input type="submit" value="Usuń"><br><br>
+   <input type="text" name="itm" placeholder="Przedmiot"> <br>
+   <input type="submit" value="dodaj"> <br><br>
+   <input type="number" name="idz" placeholder="ID"> <br>
+   <input type="text" name="itmz" placeholder="Przedmiot"> <br>
+   <input type="submit" value="zmien"> <br>
 </fieldset>
 </form>
 <?php
@@ -27,10 +26,14 @@ error_reporting(0);
 
 $id=$_POST['id'];
 $item=$_POST['itm'];
+$idz=$_POST['idz'];
+$itemz=$_POST['itmz'];
+
 $conn=mysqli_connect("localhost", 'root', '','baza');
 $kw1="SELECT id, Przedmiot from zamowienia";
 $kw2="DELETE from zamowienia where id=$id";
 $kw3="INSERT into zamowienia values (NULL,'$item')";
+$kw4="UPDATE zamowienia SET Przedmiot='$itemz' WHERE id=$idz";
 
 $wynik = mysqli_query($conn, $kw1);
 
@@ -40,6 +43,7 @@ while ($r = mysqli_fetch_row($wynik)) {
     
 }
 echo("</table>");
+
 if($id!=""){
    mysqli_query($conn, $kw2);
 }
@@ -48,8 +52,13 @@ else{
    echo("alert('Aby usunąć lub dodać przedmiot w tabeli, wypełnij właściwe pole i naciśnij przycisk poniżej')");
    echo("</script>");
 }
+
 if($item!=""){
    mysqli_query($conn, $kw3);
+}
+
+if($idz!="" && $itemz!=""){
+   mysqli_query($conn, $kw4);
 }
 mysqli_close($conn);
 ?>
